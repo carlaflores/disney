1cf700377536344fe1e53d173b2dcfe2721dfa36
Carla <carla.sflores96@gmail.com>
2021-09-10 00:08:31 -0300

    se agregaron rest controller

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Personaje;
@@ -20,6 +22,13 @@ public class Controller {
 		List<Personaje> personajes = personajeService.findAll();
 		return personajes;
 	}
+	
+	@PostMapping("/CrearPersonaje")
+	public Personaje crearPersonaje(@RequestBody Personaje personaje) {
+		personajeService.save(personaje);
+		return personaje;
+	}
+	
 	@GetMapping("/index.html")
 	public String paginaInicio() {
 		return "index.html";
